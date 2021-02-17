d71d9564d57a4f9c345d5302887fd358120c1a79
Marsel Gray <marselgray@gmail.com>
2021-02-17 13:02:10 -0500

    test

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+?>
+
+<form action="form.php" method="post">
+    <input type="text" placeholder="Enter Username"><br>
+    <input type="password" placeholder="Enter Password"><br>
+    <input type="submit">
+</form>
+
